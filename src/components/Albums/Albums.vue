eb67687e7d958b1ad110bf2e9ffac07383fd8201
Matjaz Saksida <saks072004@yahoo.com>
2024-07-09 13:09:58 +0200

    Pagination component update

--- src/components/Albums/Albums.vue
+++ src/components/Albums/Albums.vue
@@ -1,103 +1,109 @@
 <template>
-    <div class="wrapper">
-        <ComponentLoader v-if="albumsLoading" v-bind:content="'albums'" />
-        <ComponentError v-else-if="!albumsData" v-bind:content="'albums'" />
-        <ul v-else class="albums-list">
-            <li v-for="album in albumsData" v-bind:key="`album-${album.id}`" class="album-item">
-                <AlbumItem v-bind:album="album" />
-            </li>
-        </ul>
-        <PaginationComponent v-if="albumsPages > 1" v-bind:pages="albumsPages" />
-    </div>
+  <div class="wrapper">
+    <ComponentLoader v-if="albumsLoading" v-bind:content="'albums'" />
+    <ComponentError v-else-if="!albumsData" v-bind:content="'albums'" />
+    <ul v-else class="albums-list">
+      <li
+        v-for="album in albumsData"
+        v-bind:key="`album-${album.id}`"
+        class="album-item"
+      >
+        <AlbumItem v-bind:album="album" />
+      </li>
+    </ul>
+    <PaginationComponent v-if="albumsPages > 1" v-bind:pages="albumsPages" />
+  </div>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
-import ComponentLoader from '@/components/ComponentLoader'
-import AlbumItem from './AlbumItem'
-import ComponentError from '@/components/ComponentError'
-import PaginationComponent from '@/components/PaginationComponent'
+import { mapActions, mapGetters } from "vuex";
+import ComponentLoader from "@/components/ComponentLoader";
+import AlbumItem from "./AlbumItem";
+import ComponentError from "@/components/ComponentError";
+import PaginationComponent from "@/components/PaginationComponent";
 
 export default {
-    name: 'Albums',
-    components: {
-        ComponentLoader,
-        AlbumItem,
-        ComponentError,
-        PaginationComponent
-    },
-    data() {
-        return {
-            payload: {
-                count: {},
-                results: {
-                    _page: 1,
-                    _limit: 10
-                }
-            }
-        }
-    },
-    watch: {
-        '$route'(url) {
-            this.payload = {
-                count: {},
-                results: {
-                    _page: 1,
-                    _limit: 10
-                }
-            };
+  name: "AlbumsMain",
+  components: {
+    ComponentLoader,
+    AlbumItem,
+    ComponentError,
+    PaginationComponent,
+  },
+  data() {
+    return {
+      payload: {
+        count: {},
+        results: {
+          _page: 1,
+          _limit: 10,
+        },
+      },
+    };
+  },
+  watch: {
+    $route(url) {
+      this.payload = {
+        count: {},
+        results: {
+          _page: 1,
+          _limit: 10,
+        },
+      };
 
-            this.setPayload(url);
+      this.setPayload(url);
 
-            this.ACTION_ALBUMS(this.payload);
-        }
+      this.ACTION_ALBUMS(this.payload);
     },
-    methods: {
-        ...mapActions(['ACTION_ALBUMS']),
-        setPayload(route) {
-            const { uid, page } = route.query;
-            if( uid ) {
-                this.payload.count.userId = uid;
-                this.payload.results.userId = uid;
-            }
-            if( page ) this.payload.results._page = page;
-        }
+  },
+  methods: {
+    ...mapActions(["ACTION_ALBUMS"]),
+    setPayload(route) {
+      const { uid, page } = route.query;
+      if (uid) {
+        this.payload.count.userId = uid;
+        this.payload.results.userId = uid;
+      }
+      if (page) this.payload.results._page = page;
     },
-    created() {
-        if( this.$route.query.uid || this.$route.query.page ) {
-            this.setPayload(this.$route);
-        }
-
-        this.ACTION_ALBUMS(this.payload);
-
-        this.$root.$on('changePage', message => {
-            let routePath = { path: this.$route.path, query: { ...this.$route.query } };
-            if( message.pageNumber > 1 ) routePath.query = { ...routePath.query, page: message.pageNumber };
-            else delete routePath.query.page;
-            this.$router.replace(routePath);
-        });
-    },
-    computed: {
-        ...mapGetters(['albumsLoading', 'albumsData', 'albumsPages'])
+  },
+  created() {
+    if (this.$route.query.uid || this.$route.query.page) {
+      this.setPayload(this.$route);
     }
-}
+
+    this.ACTION_ALBUMS(this.payload);
+
+    this.$root.$on("changePage", (message) => {
+      let routePath = {
+        path: this.$route.path,
+        query: { ...this.$route.query },
+      };
+      if (message.pageNumber > 1)
+        routePath.query = { ...routePath.query, page: message.pageNumber };
+      else delete routePath.query.page;
+      this.$router.replace(routePath);
+    });
+  },
+  computed: {
+    ...mapGetters(["albumsLoading", "albumsData", "albumsPages"]),
+  },
+};
 </script>
 
 <style scoped>
-
 .albums-list {
-    position: relative;
-    width: auto;
-    margin: 0 -10px;
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
+  position: relative;
+  width: auto;
+  margin: 0 -10px;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
 }
 
 .album-item {
-    position: relative;
-    width: 50%;
-    padding: 10px;
+  position: relative;
+  width: 50%;
+  padding: 10px;
 }
-
 </style>
--- src/components/PaginationComponent.vue
+++ src/components/PaginationComponent.vue
@@ -1,59 +1,134 @@
 <template>
-    <div class="pagination-container">
-        <ul class="pagination-list">
-            <li v-for="(page, index) in pages" v-bind:key="`page-${index}`" v-bind:class="['page-item', { 'active': page == checkActivePage() }]">
-                <button class="app-button app-button-transparent page" v-on:click="changePage(page)">{{ page }}</button>
-            </li>
-        </ul>
-    </div>
+  <div class="pagination-container">
+    <ul class="pagination-list">
+      <li
+        v-for="(page, index) in pagination"
+        v-bind:key="`page-${index}`"
+        v-bind:class="['page-item', { active: page == checkActivePage() }]"
+      >
+        <button
+          class="app-button app-button-transparent page"
+          v-on:click="changePage(page)"
+          v-bind:disabled="page == currentPage"
+        >
+          {{ page }}
+        </button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'PaginationComponent',
-    props: {
-        pages: {
-            type: Number,
-            default: 0
-        }
+  name: "PaginationComponent",
+  props: {
+    pages: {
+      type: Number,
+      default: 0,
     },
-    methods: {
-        changePage(pageNumber) {
-            this.$root.$emit('changePage', { pageNumber: pageNumber });
-        },
-        checkActivePage() {
-            if( !this.$route.query.page ) return 1;
-            return this.$route.query.page;
+    pagesLimit: {
+      type: Number,
+      default: 5,
+    },
+  },
+  data() {
+    return {
+      pagination: [],
+    };
+  },
+  methods: {
+    changePage(pageNumber) {
+      this.$root.$emit("changePage", { pageNumber: pageNumber });
+    },
+    checkActivePage() {
+      if (!this.$route.query.page) return 1;
+      return this.$route.query.page;
+    },
+    buildPagination() {
+      const startPage = 1;
+      const prevPages = this.pagesLimit - 3;
+      const startPaginationupdate = this.pagesLimit;
+      const showMorePages = this.pagesLimit - 3;
+
+      // PAGES LESS THAN LIMIT
+      if (this.pages <= this.pagesLimit) {
+        this.pagination = [...Array(this.pages)].map(
+          (pageNum, index) => startPage + index
+        );
+        return;
+      }
+
+      // LAST PAGES
+      if (this.currentPage + showMorePages >= this.pages) {
+        const page = this.pages - this.pagesLimit + 1;
+        this.pagination = [...Array(this.pagesLimit)].map(
+          (pageNum, index) => page + index
+        );
+        this.pagination.unshift(1, "...");
+        return;
+      }
+
+      // MIDDLE PAGES
+      if (this.currentPage >= startPaginationupdate) {
+        const page = this.currentPage - prevPages;
+        this.pagination = [...Array(this.pagesLimit)].map(
+          (pageNum, index) => page + index
+        );
+        this.pagination.unshift(1, "...");
+        this.pagination.push("...", this.pages);
+        return;
+      }
+
+      // FIRST PAGES
+      this.pagination = [...Array(this.pagesLimit)].map(
+        (pageNum, index) => startPage + index
+      );
+      this.pagination.push("...", this.pages);
+    },
+  },
+  created() {
+    this.buildPagination();
+  },
+  computed: {
+    currentPage() {
+      return !this.$route.query.page ? 1 : Number(this.$route.query.page);
+    },
+  },
+  watch: {
+    "$route.query.page": {
+      handler(value) {
+        if (value) {
+          this.buildPagination();
         }
-    }
-}
+      },
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 .pagination-container {
-    position: relative;
-    width: 100%;
-    margin: 20px 0 0;
+  position: relative;
+  width: 100%;
+  margin: 20px 0 0;
 }
 
 .pagination-list {
-    position: relative;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
+  position: relative;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
 }
 
 .page {
-    padding: 5px;
-    font-size: 0.9rem;
-    color: var(--color-medium-grey);
+  padding: 5px;
+  font-size: 0.9rem;
+  color: var(--color-medium-grey);
 }
 
 .active .page {
-    color: var(--color-main);
-    font-weight: bold;
+  color: var(--color-main);
+  font-weight: bold;
 }
-
 </style>
