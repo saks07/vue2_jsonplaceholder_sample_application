4d99376048c45e8c32818746e1ae11c30012151f
Matjaz Saksida <saks072004@yahoo.com>
2024-07-09 13:10:51 +0200

    Prettier formatter updates

--- src/global_functions/index.js
+++ src/global_functions/index.js
@@ -1,23 +1,29 @@
-import axios from 'axios'
+import axios from "axios";
 
 const functions = {
-    doRequest: function(baseUrl, endpoint, method = 'get', headers = null, data = null) {
-        const options = {
-            baseURL: baseUrl,
-            url: endpoint,
-            method: method,
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        };
-        if( headers ) options.headers = { ...options.headers, ...headers };
-        if( data && Object.keys(data).length ) {
-            let key = 'params';
-            if( method === 'post' ) key = 'data';
-            options[key] = { ...data };
-        }
-        return axios(options);
+  doRequest: function(
+    baseUrl,
+    endpoint,
+    method = "get",
+    headers = null,
+    data = null
+  ) {
+    const options = {
+      baseURL: baseUrl,
+      url: endpoint,
+      method: method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    if (headers) options.headers = { ...options.headers, ...headers };
+    if (data && Object.keys(data).length) {
+      let key = "params";
+      if (method === "post") key = "data";
+      options[key] = { ...data };
     }
+    return axios(options);
+  },
 };
 
 export default functions;
--- src/store/modules/GlobalModule.js
+++ src/store/modules/GlobalModule.js
@@ -1,28 +1,40 @@
-import appConfig from '../../app_config/index.js'
-import globalFunctions from '../../global_functions/index.js'
+import appConfig from "../../app_config/index.js";
+import globalFunctions from "../../global_functions/index.js";
 
-const state = {
-
-};
+const state = {};
 
 const actions = {
-    ACTION_USERS_PROMISE: async ({}, payload) => {
-        return globalFunctions.doRequest(appConfig.api.url, appConfig.api.usersEndpoint, 'get', null, payload);
-    },
-    ACTION_ALBUMS_PROMISE: async ({}, payload) => {
-        return globalFunctions.doRequest(appConfig.api.url, appConfig.api.albumsEndpoint, 'get', null, payload);
-    },
-    ACTION_PHOTOS_PROMISE: async ({}, payload) => {
-        return globalFunctions.doRequest(appConfig.api.url, appConfig.api.photosEndpoint, 'get', null, payload);
-    }
+  ACTION_USERS_PROMISE: async ({}, payload) => {
+    return globalFunctions.doRequest(
+      appConfig.api.url,
+      appConfig.api.usersEndpoint,
+      "get",
+      null,
+      payload
+    );
+  },
+  ACTION_ALBUMS_PROMISE: async ({}, payload) => {
+    return globalFunctions.doRequest(
+      appConfig.api.url,
+      appConfig.api.albumsEndpoint,
+      "get",
+      null,
+      payload
+    );
+  },
+  ACTION_PHOTOS_PROMISE: async ({}, payload) => {
+    return globalFunctions.doRequest(
+      appConfig.api.url,
+      appConfig.api.photosEndpoint,
+      "get",
+      null,
+      payload
+    );
+  },
 };
 
-const mutations = {
+const mutations = {};
 
-};
+const getters = {};
 
-const getters = {
-
-};
-
-export default { state, actions, mutations, getters }
+export default { state, actions, mutations, getters };
